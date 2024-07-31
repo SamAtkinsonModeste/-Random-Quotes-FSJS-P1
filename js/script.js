8aec27c939eabce8d3566735c75f5c4e876712d4
Samantha Atkinson <sammodeste@yahoo.com>
2024-07-31 21:56:03 +0100

    printed quotes, and used setInterval func

--- js/script.js
+++ js/script.js
@@ -14,16 +14,13 @@ const quotes = [
   {
     quote: 'Beware; for I am fearless, and therefore powerful.',
     source: 'Frankenstein',
-    citation: 'The Creature',
     year: '1818',
-    author: 'Mary Shelly'
   },
   {
     quote: 'One day, you will be old enough to start reading fairytales again.',
     source: 'The Lion, the Witch, and the Wardrobe',
     citation: 'C.S Lewis',
     year: '1950',
-    author: 'C.S Lewis'
   },
   {
     quote:
@@ -36,16 +33,12 @@ const quotes = [
   {
     quote: "There is some good in this world, and it's worth fighting for.",
     source: 'The Two Towers',
-    citation: 'Samwise Gamgee',
     year: '1954',
-    author: 'J.R.R. Tolkien'
   },
   {
     quote:
       "Why, sometimes, I've believed as many as six impossible things before breakfast.",
     source: 'Through the Looking Glass',
-    citation: 'White Queen',
-    year: '1871',
     author: 'Lewis Carrol'
   },
   {
@@ -70,23 +63,25 @@ const quotes = [
     source: 'Harry Potter and the Chamber of Secrets',
     citation: 'Albus Dumbledore',
     year: '1998',
-    author: 'J.K. Rowling'
   }
 ];
 
-console.log(quotes);
+
 
 
 /***
  * `getRandomQuote` function
  ***/
 
-function randomQuote (arr) {
-  let randomArrIndex = Math.floor(Math.random() * arr.length);
-  return randomArrIndex;
+function randomQuote () {
+  let randomIndex = Math.floor(Math.random() * quotes.length);
+   let randomQuote = quotes[randomIndex];
+  return randomQuote;
+  
+  
 }
 
-console.log(randomQuote(quotes));
+
 
 /***
  * `randomColours` function
@@ -98,18 +93,45 @@ function randomColours () {
   let red = randomNum = Math.floor(Math.random() * 255) + 1;
   let green = randomNum = Math.floor(Math.random() * 255) + 1;
   let blue =randomNum = Math.floor(Math.random() * 255) + 1;
-  let rgb = `rgb(${red},${green}, ${blue})`;
+  let rgb = `rgba(${red},${green}, ${blue}, 0.5)`;
   return rgb;
 }
 
-console.log(randomColours());
+
 
 
 /***
  * `printQuote` function
 ***/
+function printQuote() {
 
+  // Colours for BODY and Quote-Box
+const bgColour = randomColours();
+ document.getElementsByTagName('body')[0].style.background = `${bgColour}`; 
+ document.getElementById('quote-box').style.background = `linear-gradient(325deg, rgb(0,0,0) -75%, ${bgColour} 15%, rgb(0,0,0) 400%)`;
 
+ //Building Quote with HTML
+ const randomQuotes = randomQuote();
+ let quotesHTML= `<p class="quote">${randomQuotes.quote}</p><p class="source">${randomQuotes.source}`;
+
+    if (randomQuotes.citation){
+       quotesHTML += `<span class="citation">${randomQuotes.citation}</span>` 
+    }
+
+    if (randomQuotes.year){
+      quotesHTML += `<span class="year">${randomQuotes.year}</span>` 
+   }
+
+   if (randomQuotes.author){
+    quotesHTML += `<span class="author">${randomQuotes.author}</span>` 
+ }
+
+ quotesHTML +=`</p>`;
+  document.getElementById('quote-box').innerHTML = quotesHTML;
+}
+
+document.getElementsByTagName('body')[0].addEventListener('DOMContentLoaded', printQuote());
+setInterval(printQuote, 10000);
 
 /***
  * click event listener for the print quote button
